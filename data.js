268fa3a80d65deb987991beb3be9f002221e9f65
pqppq <yutatsu0119@gmail.com>
2023-04-01 12:00:44 +0900

    chore: add created

--- data.js
+++ data.js
@@ -5,9 +5,9 @@ export const data = {
 		{"githubLogin": "sSchmidt", "name": "Scot Schmidt"},
 	],
 	photos: [
-		{"id": 1, "name": "Droppping the Heart Chute", description: "The heart chute is one of my favorite chutes", "category": "ACTION", "githubUser": "gPlake"},
-		{"id": 2, "name": "Enjoying the sunshine", "category": "SELFIE", "githubUser": "sSchmidt"},
-		{"id": 3, "name": "Gunbarrel 25", description: "24 laps on gunbarrel today", "category": "LANDSCAPE", "githubUser": "sSchmidt"},
+		{"id": 1, "name": "Droppping the Heart Chute", description: "The heart chute is one of my favorite chutes", "category": "ACTION", "githubUser": "gPlake", "created": "3-28-1977"},
+		{"id": 2, "name": "Enjoying the sunshine", "category": "SELFIE", "githubUser": "sSchmidt", "created": "1-2-1985"},
+		{"id": 3, "name": "Gunbarrel 25", description: "24 laps on gunbarrel today", "category": "LANDSCAPE", "githubUser": "sSchmidt", "created": "2018-04-15T19:09:57.308Z"},
 	],
 	tags: [
 		{"photoId": 1, "userId": "gPlake"},
